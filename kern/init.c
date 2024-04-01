8e8ca3eee5f57c82b7f7b14937cd9b1bec9aba37
Zohar Lee <root@leafvmaple@gmail.com>
2024-04-01 09:44:04 +0800

    feat: update message

--- kern/init.c
+++ kern/init.c
@@ -4,6 +4,5 @@ int kern_init(void) __attribute__((noreturn));
 
 int kern_init(void) {
     cons_init();
-    cprintf("Loading Zonix System...");
-    while (1);
+    cprintf("Zonix OS is Loading...");
 }
