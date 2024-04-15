a620298a8eaee367f526386380cb62e42706d04d
Zohar Lee <root@leafvmaple@gmail.com>
2024-04-16 01:22:33 +0800

    feat: update 20240416

--- include/com/trap.h
+++ include/com/trap.h
@@ -1,20 +1,21 @@
 #pragma once
 #include "def.h"
 
-struct pushregs {
+struct trap_regs {
     uint32_t reg_edi;
     uint32_t reg_esi;
     uint32_t reg_ebp;
-    uint32_t reg_oesp; /* Useless */
+    uint32_t unused; /* Useless */
     uint32_t reg_ebx;
     uint32_t reg_edx;
     uint32_t reg_ecx;
     uint32_t reg_eax;
 };
 
-typedef struct gate_desc_table {
-    uint16_t idt_lim;   // Limit
-    uintptr_t pd_base;  // Base address
-} gate_desc_table __attribute__((packed));
+struct trap_frame {
+    struct trap_regs tf_regs;
+
+    uint32_t tf_err;
+};
 
 void trap_init(void);
--- kern/trap/trap.c
+++ kern/trap/trap.c
@@ -2,19 +2,18 @@
 #include "arch/x86/x86.h"
 
 typedef struct gate_desc {
-    unsigned gd_off_15_0 : 16;   // low 16 bits of offset in segment
-    unsigned gd_ss : 16;         // segment selector
-    unsigned gd_args : 5;        // # args, 0 for interrupt/trap gates
-    unsigned gd_rsv1 : 3;        // reserved(should be zero I guess)
-    unsigned gd_type : 4;        // type(STS_{TG,IG32,TG32})
-    unsigned gd_s : 1;           // must be 0 (system)
-    unsigned gd_dpl : 2;         // descriptor(meaning new) privilege level
-    unsigned gd_p : 1;           // Present
+    unsigned gd_off_15_0  : 16;  // low 16 bits of offset in segment
+    unsigned gd_ss        : 16;  // segment selector
+    unsigned gd_args      : 5;   // # args, 0 for interrupt/trap gates
+    unsigned gd_rsv1      : 3;   // reserved(should be zero I guess)
+    unsigned gd_type      : 4;   // type(STS_{TG,IG32,TG32})
+    unsigned gd_s         : 1;   // must be 0 (system)
+    unsigned gd_dpl       : 2;   // descriptor(meaning new) privilege level
+    unsigned gd_p         : 1;   // Present
     unsigned gd_off_31_16 : 16;  // high bits of offset in segment, always 0
 } gate_desc;
 
 extern gate_desc __idt[256];
-extern gate_desc_table __idtdesc;
 extern uintptr_t __vectors[];
 
 #ifdef _ASM_
@@ -49,10 +48,8 @@ __asm__ (               \
 void trap_init() {
     for (int i = 0; i < 256; i++)
         SET_TRAP_GATE(i, __vectors[i]);
-
-    lidt(&__idtdesc);
 }
 
-void trap(struct trapframe *tf) {
+void trap(struct trap_frame *tf) {
 
 }
