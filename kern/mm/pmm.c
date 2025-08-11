a787c553ee31f537c8f29d1c0a52c76f99d0489f
Zohar Lee <leafvmaple@gmail.com>
2025-08-11 17:20:10 +0800

    refactor: move page directory printing function to VMM

--- kern/mm/pmm.c
+++ kern/mm/pmm.c
@@ -27,9 +27,6 @@ const pmm_manager* pmm_mgr = NULL;
 PageDesc *pages = NULL;
 uint32_t npage = 0;
 
-pte_t *const vpt = (pte_t *)VPT;
-pde_t *const vpd = (pde_t *)PG_ADDR(PDX(VPT), PDX(VPT), 0);
-
 static inline uintptr_t page2pa(PageDesc *page) {
     return (page - pages) << PG_SHIFT;
 }
@@ -74,47 +71,6 @@ pte_t* get_pte(pde_t* pgdir, uintptr_t la, int create) {
         *pdep = pa | PTE_USER;
     }
     return (pte_t*)K_ADDR(PDE_ADDR(*pdep)) + PTX(la);
-}
-
-static int get_pgtable_items(size_t start, size_t limit, uintptr_t *table, size_t *left, size_t *right) {
-    if (start >= limit) {
-        return 0;
-    }
-    while (start < limit && !(table[start] & PTE_P)) {
-        start++;
-    }
-    if (start < limit) {
-		*left = start;
-        int perm = (table[start++] & PTE_USER);
-        while (start < limit && (table[start] & PTE_USER) == perm) {
-            start++;
-        }
-		*right = start;
-        return perm;
-    }
-    return 0;
-}
-
-static const char* perm2str(int perm) {
-    static char str[4];
-    str[0] = (perm & PTE_U) ? 'u' : '-';
-    str[1] = (perm & PTE_P) ? 'r' : '-';
-    str[2] = (perm & PTE_W) ? 'w' : '-';
-    str[3] = '\0';
-    return str;
-}
-
-void print_pgdir() {
-    cprintf("-------------------- BEGIN --------------------\n");
-    size_t left, right = 0, perm;
-    while ((perm = get_pgtable_items(right, PDE_NUM, vpd, &left, &right)) != 0) {
-        cprintf("PDE(%03x) %08x-%08x %08x %s\n", right - left, left * PT_SIZE, right * PT_SIZE, (right - left) * PT_SIZE, perm2str(perm));
-        size_t l, r = left * PTE_NUM;
-        while ((perm = get_pgtable_items(r, right * PTE_NUM, vpt, &l, &r)) != 0) {
-            cprintf("  |-- PTE(%05x) %08x-%08x %08x %s\n", r - l, l * PG_SIZE, r * PG_SIZE, (r - l) * PG_SIZE, perm2str(perm));
-        }
-    }
-    cprintf("--------------------- END ---------------------\n");
 }
 
 static void page_init() {
